2b8eb286e4113b1a80229bb8b449038679972f67
rpascoato <rapha367@live.com>
2021-08-19 14:20:50 -0300

    BoardExcecao e programando defensivamente

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 	private Peca[][] pecas;
 	
 	public Board(int fileiras, int colunas) {
+		if (fileiras <1 || colunas < 1) {
+			throw new BoardExcecao("Erro criando tabuleiro: é necessario que haja pelo menos uma linha e uma coluna");
+		}
 		this.fileiras = fileiras;
 		this.colunas = colunas;
 		pecas = new Peca[fileiras][colunas];
@@ -16,28 +19,44 @@ public class Board {
 		return fileiras;
 	}
 
-	public void setFileiras(int fileiras) {
-		this.fileiras = fileiras;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
-
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
 	
 	public Peca peca (int fileira, int coluna) {
+		if (!posicaoExiste(fileira, coluna)) {
+			throw new BoardExcecao("Posição não está no tabuleiro");
+		}
 		return pecas [fileira][coluna];		
 	}
 	
 	public Peca peca(Posicao posicao) {
+		if (!posicaoExiste(posicao)) {
+			throw new BoardExcecao("Posição não está no tabuleiro");
+		}
 		return pecas[posicao.getFileira()][posicao.getColuna()];
 	}
 	
 	public void pecaLugar (Peca peca, Posicao posicao) {
+		if (haUmaPeca(posicao)) {
+			throw new BoardExcecao("Já existe uma peça nessa posição " + posicao);
+		}
 		pecas[posicao.getFileira()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+	
+	private boolean posicaoExiste (int fileira, int coluna) {
+		return fileira >= 0 && fileira < fileiras && coluna >= 0 && coluna < colunas;
+	}
+	
+	public  boolean posicaoExiste (Posicao posicao) {
+		return posicaoExiste(posicao.getFileira(), posicao.getColuna());
+	}
+	
+	public boolean haUmaPeca (Posicao posicao) {
+		if (!posicaoExiste(posicao)) {
+			throw new BoardExcecao("Posição não está no tabuleiro");
+		}
+		return peca(posicao) != null;
 	}
 }
---
+++ src/boardgame/BoardExcecao.java
@@ -0,0 +1,13 @@
+package boardgame;
+
+public class BoardExcecao extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExcecao (String msg) {
+		super (msg);
+	}
+	
+	
+
+}
